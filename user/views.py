bf0455e8c43d47e8db1609dac0313358753ea017
Olha Yukhnenko <pyolh36@gmail.com>
2026-03-30 14:15:58 +0300

    fix duplicates in .gitignore file and remove unused imports

--- user/views.py
+++ user/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import get_user_model
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
